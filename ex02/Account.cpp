dd30538a3812eae65d3d8ff6086ca8d3a4f1a774
Azat Faezov <farscince@gmail.com>
2021-10-11 23:14:55 +0300

    ex02 in progress

---
+++ ex02/Account.cpp
@@ -0,0 +1,14 @@
+#include "Account.hpp"
+
+void Account::_displayTimestamp() {
+	std::time_t t = std::time(0);   // get time now
+	std::tm* now = std::localtime(&t);
+	std::cout << '['
+			<< std::setw(2) << std::setfill('0') << now->tm_year + 1900
+			<< std::setw(2) << std::setfill('0') << now->tm_mon + 1
+			<< std::setw(2) << std::setfill('0') << now->tm_mday << '_'
+			<< std::setw(2) << std::setfill('0') << now->tm_hour
+			<< std::setw(2) << std::setfill('0') << now->tm_min
+			<< std::setw(2) << std::setfill('0') << now->tm_sec << ']'
+			<< std::endl;
+}
